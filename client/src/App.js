3d4fa228ffbfcbc3a3f3b8c16e82a8b8a31afad2
zewge-3 <zewge@student.chalmers.se>
2025-09-02 14:45:30 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useState } from "react";
 
@@ -42,28 +41,6 @@ function App() {
           <pre>{JSON.stringify(analysis, null, 2)}</pre>
         </div>
       )}
-    </div>
-  );
-}
-
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
